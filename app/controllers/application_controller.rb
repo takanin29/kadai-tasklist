9d0c6a005df8949e21b94c2835dead210a355434
takanin29 <takanin29@gmail.com>
2019-09-02 15:45:31 +0000

    CRUD tasks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,57 @@
 class ApplicationController < ActionController::Base
+    def index
+        @tasks = tasks.all
+    end
+    
+    def show
+        @task = task.find(params[:id])
+    end
+    
+    def new
+        @task = Task.new
+    end
+    
+    def create
+        @task = Task.new(task_params)
+        
+        if @task.save
+            flash[:success] = 'Task が正常に入力されました'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'Task が正しく入力されませんでした'
+            render :new
+        end
+    end
+    
+    def edit
+        @task = Task.find(params[:id])
+    end
+    
+    def update
+        @task = Task.find(params[:id])
+        
+        if @task.update(task_params)
+            flash[:success] = 'Task は正常に更新されました'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'Task は更新させませんでした'
+            render :edit
+        end
+    end
+    
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        
+        flash[:success] = 'Task は正常に削除されました'
+        redirect_to tasks_url
+    end
+    
+    private
+    
+    #Strong Parameter
+    def task_params
+        params.require(:task).permit(:content)
+    end
+    
 end
