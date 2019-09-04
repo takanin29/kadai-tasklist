3bebe13013d41a8e98775afeed4f36d33a32e0b4
takanin29 <takanin29@gmail.com>
2019-09-04 13:50:25 +0000

    add column revised v02

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_125849) do
+ActiveRecord::Schema.define(version: 2019_09_04_124013) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
