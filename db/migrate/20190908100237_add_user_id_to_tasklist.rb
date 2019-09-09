59b2c44ccb76f4addd5e775525aae2666cb7b3a7
takanin29 <takanin29@gmail.com>
2019-09-09 16:14:45 +0000

    user login

--- db/migrate/20190908100237_add_user_id_to_tasklist.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToTasklist < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :tasklists, :user_id, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_124013) do
+ActiveRecord::Schema.define(version: 2019_09_08_100237) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.bigint "user_id_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+    t.index ["user_id_id"], name: "index_tasks_on_user_id_id"
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+  end
+
+  add_foreign_key "tasks", "users"
 end
