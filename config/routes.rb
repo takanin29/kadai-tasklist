f990053e6ef193d25b3e2f2f23cb98075206a0f5
takanin29 <takanin29@gmail.com>
2019-09-09 16:41:14 +0000

    view?

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
  
  get 'signup', to: 'users#new'
  resources :users, only: [:index, :show, :new, :create]
+ 
+ resources :tasks, only: [:create, :edit, :destroy]
 end
